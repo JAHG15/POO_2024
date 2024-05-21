8d3fe81aa3bd1b676ab575de4e0c8a78fefd013d
JAHG15 <joshuahego@gmail.com>
2024-05-21 10:59:35 -0600

    Si

---
+++ Parcial1/6_Ejercicios_Repaso/Ejercicio6.py
@@ -0,0 +1,50 @@
+numero1=1;
+for numero in range (1,11):
+    multi=numero*numero1;
+    print(f"{numero1} X {numero} = {multi} ");
+print("============================");
+numero2=2;
+for numero in range (1,11):
+    multi=numero*numero2;
+    print(f"{numero2} X {numero} = {multi} ");
+print("============================");
+numero3=3;
+for numero in range (1,11):
+    multi=numero*numero3;
+    print(f"{numero3} X {numero} = {multi} ");
+print("============================");
+numero4=4;
+for numero in range (1,11):
+    multi=numero*numero4;
+    print(f"{numero4} X {numero} = {multi} ");
+print("============================");
+numero5=5;
+for numero in range (1,11):
+    multi=numero*numero5;
+    print(f"{numero5} X {numero} = {multi} ");
+print("============================");
+numero6=6;
+for numero in range (1,11):
+    multi=numero*numero6;
+    print(f"{numero6} X {numero} = {multi} ");
+print("============================");
+numero7=7;
+for numero in range (1,11):
+    multi=numero*numero7;
+    print(f"{numero7} X {numero} = {multi} ");
+print("============================");
+numero8=8;
+for numero in range (1,11):
+    multi=numero*numero8;
+    print(f"{numero8} X {numero} = {multi} ");
+print("============================");
+numero9=9;
+for numero in range (1,11):
+    multi=numero*numero9;
+    print(f"{numero9} X {numero} = {multi} ");
+print("============================");
+numero10=10;
+for numero in range (1,11):
+    multi=numero*numero10;
+    print(f"{numero10} X {numero} = {multi} ");
+print("============================");
