7af60830d3d3a373f5e9765e6489a2d6e44aff26
JAHG15 <joshuahego@gmail.com>
2024-05-24 11:05:25 -0600

    Si

--- Parcial1/6_Ejercicios_Repaso/Ejercicio1.py
+++ Parcial1/6_Ejercicios_Repaso/Ejercicio1.py
@@ -4,3 +4,4 @@ print(f"El pais es: {pais}");
 print(f"El continente es: {continente}");
 
 print(f"El tipo de dato de la variable pais es: {type(pais)} y el tipo de datos de la variable continente es: {type(continente)}");
+x=33;
