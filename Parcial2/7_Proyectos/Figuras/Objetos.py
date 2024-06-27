055b32ba6fcd7f46e76bd7e5ac9dbe0bf6a6cc8a
JAHG15 <joshuahego@gmail.com>
2024-06-27 09:56:36 -0600

    ER Parcial 2 Figuras Geometricas

---
+++ Parcial2/7_Proyectos/Figuras/Objetos.py
@@ -0,0 +1,9 @@
+from Clases import *
+rectangulo1=Rectangulo(10,5)
+print(f"Rectangulo\n Area: {rectangulo1.CalcularArea()}")
+
+circulo1=Circulo(20,3.1416)
+print(f"Circulo\n Area: {circulo1.CalcularArea()}")
+
+Triangulo1=Triangulo(20,10)
+print(f"Area: {Triangulo1.CalcularArea()}")
